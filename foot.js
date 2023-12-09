6de897bab9d1e362ccb10b4154983ef19cac822a
bettersoft <bettersoft@qq.com>
2023-12-09 23:02:02 +0800

    adjust

--- foot.js
+++ foot.js
@@ -1,3 +1,4 @@
 document.write(`
-    © 2023 老家河南 版权所有
+    <p class="company"><a href="#">关于我们</a>  <a href="#">反馈问题</a>  <a href="#">投稿编辑</a></p>
+    <p class="copyright">© 2023 老家河南 版权所有</p>
     `)
