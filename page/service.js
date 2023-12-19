4c809ae60efd3faa834c3be1f26fe3e9854dcaa9
bettersoft <bettersoft@qq.com>
2023-12-19 17:06:09 +0800

    bbb

--- page/service.js
+++ page/service.js
@@ -1,12 +1,12 @@
 document.write(`
     <div class="service">
         <label>携程旅行</label>
-        <a href="https://m.ctrip.com/html5/flight/home?ouid=kfptpctool&allianceid=4090906&sid=32672662">航班机票</a>
+        <a href="https://m.ctrip.com/html5/flight/home?ouid=kfptpctool&allianceid=4090906&sid=32672662">特价机票</a>
          ※ 
         <a href="https://m.ctrip.com/tangram/ticket?ouid=kfptpctool&allianceid=4090906&sid=32672662">景点门票</a>
          ※ 
-        <a href="https://m.ctrip.com/webapp/hotel?ouid=kfptpctool&allianceid=4090906&sid=32672662">酒店预定</a>
+        <a href="https://m.ctrip.com/webapp/hotel?ouid=kfptpctool&allianceid=4090906&sid=32672662">酒店宾馆</a>
          ※ 
-        <a href="https://m.ly.com/trainnin1/touch/#/index?refid=2000122267">查火车票</a>
+        <a href="https://m.ly.com/trainnin1/touch/#/index?refid=2000122267">同程火车</a>
     </div>
 `)
