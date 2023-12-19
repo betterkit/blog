07cfec5d46ddc8be9d2b6d1050fcb9fba8998be7
bettersoft <bettersoft@qq.com>
2023-12-19 16:13:09 +0800

    add ads

---
+++ page/service.js
@@ -0,0 +1,12 @@
+document.write(`
+    <div class="service">
+        <label>携程旅行</label>
+        <a href="https://m.ctrip.com/html5/flight/home?ouid=kfptpctool&allianceid=4090906&sid=32672662">机票预定</a>
+         ※ 
+        <a href="https://m.ctrip.com/tangram/ticket?ouid=kfptpctool&allianceid=4090906&sid=32672662">景点门票</a>
+         ※ 
+        <a href="https://m.ctrip.com/webapp/hotel?ouid=kfptpctool&allianceid=4090906&sid=32672662">酒店预定</a>
+         ※ 
+        <a href="https://m.ctrip.com/webapp/cw/car/isd/Home.html?ouid=kfptpctool&allianceid=4090906&sid=32672662">我要租车</a>
+    </div>
+`)
