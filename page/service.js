e053d57eac9c6f372d1a0402ad69fd4688ce3e45
bettersoft <bettersoft@qq.com>
2023-12-19 16:49:32 +0800

    weitiao

--- page/service.js
+++ page/service.js
@@ -1,18 +1,12 @@
 document.write(`
     <div class="service">
         <label>携程旅行</label>
-        <a href="https://m.ctrip.com/html5/flight/home?ouid=kfptpctool&allianceid=4090906&sid=32672662">特价机票</a>
+        <a href="https://m.ctrip.com/html5/flight/home?ouid=kfptpctool&allianceid=4090906&sid=32672662">航班机票</a>
          ※ 
         <a href="https://m.ctrip.com/tangram/ticket?ouid=kfptpctool&allianceid=4090906&sid=32672662">景点门票</a>
          ※ 
-        <a href="https://m.ctrip.com/webapp/hotel?ouid=kfptpctool&allianceid=4090906&sid=32672662">预定酒店</a>
-    </div>
-    <div class="service">
-        <label>同程旅行</label>
-        <a href="https://m.ly.com/kylintouch/cindex?refid=2000122267">国内机票</a>
+        <a href="https://m.ctrip.com/webapp/hotel?ouid=kfptpctool&allianceid=4090906&sid=32672662">酒店预定</a>
          ※ 
-        <a href="https://m.ly.com/tiflightnfe/?refid=2000122267">国际机票</a>
-         ※ 
-        <a href="https://m.ly.com/trainnin1/touch/#/index?refid=2000122267">火车票</a>
+        <a href="https://m.ly.com/trainnin1/touch/#/index?refid=2000122267">查火车票</a>
     </div>
 `)
