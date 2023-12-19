fe8ddcfa8a3e2895e4f3c50c050a8490d3c39e57
bettersoft <bettersoft@qq.com>
2023-12-19 16:25:03 +0800

    aaa

--- page/service.js
+++ page/service.js
@@ -6,7 +6,5 @@ document.write(`
         <a href="https://m.ctrip.com/tangram/ticket?ouid=kfptpctool&allianceid=4090906&sid=32672662">景点门票</a>
          ※ 
         <a href="https://m.ctrip.com/webapp/hotel?ouid=kfptpctool&allianceid=4090906&sid=32672662">酒店预定</a>
-         ※ 
-        <a href="https://m.ctrip.com/webapp/cw/car/isd/Home.html?ouid=kfptpctool&allianceid=4090906&sid=32672662">我要租车</a>
     </div>
 `)
